f51ba1ad8e68a0f3728d1fd46abfeb54cb393b7c
jos <85748945+josuefcp17@users.noreply.github.com>
2021-11-18 11:35:15 -0500

    Add files via upload

---
+++ liberman.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+#include "users.h"
+#include "alum.h"
+#include "profe.h"
+#include "admin.h"
+#include "menu.h"
+
